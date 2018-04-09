2c277505a0ce0dab6f43befc95e184540a39c790
Thorben Menne <mennthor@aol.com>
2018-04-09 14:46:56 +0200

    Timing and fix in outfile creation

--- 01-create_hese_equatorial_maps.py
+++ 01-create_hese_equatorial_maps.py
@@ -10,16 +10,20 @@ Find pass2 scan files at:
 from __future__ import print_function, division
 import os
 from glob import glob
+import datetime
 
 from myi3scripts.hese import make_healpy_map_from_HESE_scan
+
+
+print("Start: {}\n".format(datetime.datetime.utcnow()))
 
 inpath = os.path.join("/data", "ana", "Diffuse", "HESE", "Pass2_reconstruction",
                       "reconstruction_tracks", "Run*")
 folders = glob(inpath)
 
 outpath = os.path.join("/home", "user", "tmenne", "analysis", "hese_scan_maps")
-outfiles = map(lambda p, name: os.path.join(p, name),
-               zip(outpath, map(os.path.basename, folders)))
+outfiles = map(lambda name: os.path.join(outpath, name),
+               map(os.path.basename, folders))
 
 icemodel = "SpiceMie"
 scan_str = "{}_nside????.i3.bz2".format(icemodel)
@@ -29,12 +33,12 @@ for i, folder in enumerate(folders):
     print("Working on scan:\n  {}".format(folder))
     print("Output file path:\n  {}".format(outfiles[i]))
 
-    # make_healpy_map_from_HESE_scan(infolder=folder,
+    # make_healpy_map_from_HESE_scan(infolder=infolder,
     #                                scan_file_str=scan_str,
-    #                                outfile=outfiles[i],
+    #                                outfile=outfile,
     #                                coord="equ",
     #                                outfmt="json",
-    #                                smooth_sigma=1.
+    #                                smooth_sigma=1.,
     #                                )
 
-print("Done!")
+print("\nDone: {}".format(datetime.datetime.utcnow()))
