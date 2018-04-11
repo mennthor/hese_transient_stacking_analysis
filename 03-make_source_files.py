63566b2290bab0050d27d1638782d7dcac3ed2e4
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-04-11 05:40:45 -0500

    Script to make source files in JSON with the compact info needed for analysis.

--- 03-make_source_files.py
+++ 03-make_source_files.py
@@ -7,16 +7,75 @@ which sample each on belongs to.
 
 import os
 import json
+from glob import glob
 import gzip
 import numpy as np
 import astropy.time as astrotime
 
+from _paths import PATHS
 from skylab.datasets import Datasets
 
+src_path = os.path.join(PATHS.local, "hese_pass2_scan_maps")
+runlist_path = os.path.join(PATHS.local, "runlists")
 
-PATHS = json.load(open("./WORK_DIRS.json"))
+outpath = os.path.join(PATHS.local, "source_list")
+if not os.path.isdir(outpath):
+    os.makedirs(outpath)
 
-src_path = os.path.join(PATHS["local"], "hese_scan_maps")
-runlist_path = os.path.join(PATHS["local"], "runlists")
+# Load runlists
+runlist_files = sorted(glob(os.path.join(runlist_path, "*")))
+runlists = {}
+for rl_file in runlist_files:
+    name = os.path.splitext(os.path.basename(rl_file))[0]
+    with open(rl_file) as _f:
+        print("Load runlist for sample {} from\n  {}".format(name, rl_file))
+        runlists[name] = json.load(_f)
 
-# TODO: Load runlists and sources
+# Load sources up to HESE 6yr, list from:
+#   https://wiki.icecube.wisc.edu/index.php/Analysis_of_pre-public_alert_HESE/EHE_events#HESE
+# Last Run ID is 127853 from late 86V (2015) run, next from 7yr is 128290
+# glob("./Run0012[1-7]*.json.gz") + glob("./Run0011*.json.gz")
+src_files = sorted(
+    glob(os.path.join(src_path, "Run0011*.json.gz")) +
+    glob(os.path.join(src_path, "Run0012[0-7]*.json.gz"))
+    )
+
+sources = []
+for src_file in src_files:
+    with gzip.open(src_file) as _f:
+        src_dict = json.load(_f)
+        # Build a compact version with all relevant infos
+        src_i = {}
+        for key in ["run_id", "event_id", "mjd"]:
+            src_i[key] = src_dict[key]
+        # Store best fit from direct local trafo and map maximum
+        src_i["ra"] = src_dict["bf_equ"]["ra"]
+        src_i["dec"] = src_dict["bf_equ"]["dec"]
+        src_i["ra_map"] = src_dict["bf_equ_pix"]["ra"]
+        src_i["dec_map"] = src_dict["bf_equ_pix"]["dec"]
+        # Also store the path to the original file which contains the skymap
+        src_i["map_path"] = src_file
+        sources.append(src_i)
+        print("Loaded HESE source from run {}".format(src_i["run_id"]))
+
+print("Number of considered sources: {}".format(len(src_files)))
+sources = np.array(sources)
+
+# Match sources with their seasons and store to JSON
+# We could just copy the list from the wiki here, but let's just check with
+# the runlists
+sources_per_sam = {}
+src_t = np.array([src_["mjd"] for src_ in sources])
+for name, runs in runlists.items():
+    print("Match sources for sample {}".format(name))
+    tmin = np.amin([astrotime.Time(r["good_tstart"]).mjd for r in runs])
+    tmax = np.amax([astrotime.Time(r["good_tstop"]).mjd for r in runs])
+    t_mask = (src_t >= tmin) & (src_t <= tmax)
+
+    # Store all sources for the current sample
+    sources_per_sam[name] = sources[t_mask].tolist()
+
+assert sum(map(len, sources_per_sam)) == len(sources)
+
+with open(os.path.join(outpath, "source_list.json"), "w") as outf:
+    json.dump(sources_per_sam, fp=outf, indent=2)
