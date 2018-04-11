e3d3d1b719e83f986cb29a059bc6c874a2777006
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-04-11 03:31:39 -0500

    Added mini class to handle working dirs for branches

---
+++ _paths.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+"""
+Working directories for each branch.
+By using the paths stored here consistently, we can set new working directories
+for each branch, without interfering with previous results.
+
+In a script use `from PATHS import PATHS` and eg. `local_path = PATHS.local`.
+Get all available paths with `print(PATHS)`.
+```
+"""
+
+import os as _os
+from git import Repo as _Repo
+
+
+class _Paths(object):
+    """
+    Class to acces paths via it's attributes.
+    Code adopted from scipy.optimize.OptimizeResult.
+    """
+    def __init__(self, d):
+        self._d = d
+
+    def __getattr__(self, name):
+            try:
+                return self._d[name]
+            except KeyError:
+                raise AttributeError(name)
+
+    def __setattr__(self, name, val):
+        if name != "_d":
+            raise RuntimeError("PATHS is readonly.")
+        else:
+            # We could still overwrite _d, but who does that?
+            super(_Paths, self).__setattr__(name, val)
+
+    def __repr__(self):
+        m = max(map(len, list(self._d.keys()))) + 1
+        return '\n'.join([name.rjust(m) + ': ' + path
+                          for name, path in self._d.items()])
+
+
+# Insert the current branch name to automatically switch to a new work dir
+_repo = _Repo("./")
+_BRANCH_NAME = _repo.active_branch.name
+
+_paths = {
+    "local": _os.path.join("/home", "tmenne", "analysis",
+                           "hese_transient_stacking_analysis",
+                           "out_" + _BRANCH_NAME),
+    "data": _os.path.join("/data", "user", "tmenne",
+                          "hese_transient_stacking_analysis",
+                          "rawout_" + _BRANCH_NAME),
+    "jobs": _os.path.join("/home", "tmenne", "analysis",
+                          "hese_transient_stacking_analysis",
+                          "jobfiles_" + _BRANCH_NAME),
+}
+
+PATHS = _Paths(_paths)
