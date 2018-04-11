0987fecac41054d1d96a5d5713e85ca6af9bc5b6
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-04-11 10:27:25 -0500

    Apdapted paths to make maps from original HESE scans

--- 01-create_hese_equatorial_maps.py
+++ 01-create_hese_equatorial_maps.py
@@ -4,8 +4,8 @@
 Make complete healpy logLLH maps from HESE scans and save in npy or json as
 simple arrays.
 
-Find pass2 scan files at:
-    /data/ana/Diffuse/HESE/Pass2_reconstruction/reconstruction_tracks
+Find original HESE rescan files at:
+    /data/ana/IC79/starting-event/IC170922A_scans_all_historrical_events
 
 This script processes all track maps, converts them to an equatorial map,
 applies 1° smoothing in normal space and normalizes the map to have integral 1
@@ -23,9 +23,13 @@ from myi3scripts.hese import make_healpy_map_from_HESE_scan
 
 print("Start: {}\n".format(datetime.datetime.utcnow()))
 
-inpath = os.path.join("/data", "ana", "Diffuse", "HESE", "Pass2_reconstruction",
-                      "reconstruction_tracks", "Run*")
+inpath = os.path.join("/data", "ana", "IC79", "starting-event",
+                      "IC170922A_scans_all_historrical_events",
+                      "??????.i3.bz2_event0000")
 folders = sorted(glob(inpath))
+print("Found {} scan folders:".format(len(folders)))
+for folder in folders:
+    print("  {}".format(folder))
 
 outpath = os.path.join(PATHS.local, "hese_scan_maps")
 if not os.path.isdir(outpath):
@@ -34,13 +38,13 @@ outfiles = map(lambda name: os.path.join(outpath, name),
                map(os.path.basename, folders))
 
 icemodel = "SpiceMie"
-scan_str = "{}_nside0008.i3.bz2".format(icemodel)
+scan_str = "step0[0-9]_{}_nside????.i3.bz2".format(icemodel)
 
 # Work on multiple maps at once. If module not available, loop over all files
-N_JOBS = 20
+N_JOBS = len(folders)
 with ProcessPoolExecutor(max_workers=N_JOBS) as executor:
     processes = []
-    for infolder, outfile in zip(folders, outfiles):
+    for infolder, outfile in zip(folders, outfiles)[1:]:
         processes.append(
             executor.submit(
                 make_healpy_map_from_HESE_scan,
